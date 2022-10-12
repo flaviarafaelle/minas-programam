53776a7878c8f4e623765bd4d0321e5c339214e9
flavinha <flaviarlimasantos@gmail.com>
2022-10-12 10:32:51 -0300

    fix: corrigindo variável

--- lógica de programação/atividadeAula2.js
+++ lógica de programação/atividadeAula2.js
@@ -1,8 +1,8 @@
 // Crie um algoritmo que calcule a velocidade média de um veículo e retorne com resultado. 
 // Fórmula: Vm(Velocidade média) = distância/tempo 
 
-let distancia = 1200;
-let tempo = 10;
+const distancia = 1200;
+const tempo = 10;
 
 const divisao = distancia / tempo;
 
