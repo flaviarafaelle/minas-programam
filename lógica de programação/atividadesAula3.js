9651873850f0f77450ac1d1e9a2d33dbbcbdce9a
flavinha <flaviarlimasantos@gmail.com>
2022-10-13 21:01:57 -0300

    feat: aula 3

---
+++ lógica de programação/atividadesAula3.js
@@ -0,0 +1,24 @@
+// = -> atribuição
+
+//== ->  igualdade -> compara apenas o valor
+
+//=== -> igualdade -> compara o valor e o tipo
+
+//!= -> diferença -> verifica apenas se os valores são iguais
+
+//!== -> diferença -> verifica o valor e o tipo 
+
+let ch = "choveu"
+let s = "fez sol"
+
+if (ch == "choveu") {
+    console.log ("hoje choveu")
+} else {
+    console.log ("hj n choveu")
+}
+
+/*
+if (condicao) {
+    resultado p esta condicao
+}
+*/
