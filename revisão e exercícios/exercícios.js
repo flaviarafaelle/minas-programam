1b588009acbc6b188959941a888be0a386822cd0
flavinha <flaviarlimasantos@gmail.com>
2022-10-17 21:39:49 -0300

    feat: exercicios

---
+++ revisão e exercícios/exercícios.js
@@ -0,0 +1,49 @@
+// 1. Armazene na variável nome colaboradora o nome Maria
+
+let nomeColaboradora = "Maria";
+
+//console.log(`A colaboradora se chama ${nomeColaboradora}`);
+
+// 2. Armazene uma soma entre 2 números
+
+let num1 = 10;
+let num2 = 3;
+let lula = num1 + num2;
+
+//console.log(`A soma é ${lula}`)
+
+// 3. Armazene separadamente, TODOS os tipos de dados que vocês aprenderam até o momento; Utilize o console.log(typeof(nomeDaVariavel)).
+
+
+const string = "banana"
+const inteiro = 13
+const float = 1.3
+const booleanoTrue = true
+const booleanoFalse = false
+/*
+console.log(typeof(string))
+console.log(typeof(inteiro))
+console.log(typeof(float))
+console.log(typeof(booleanoTrue))
+console.log(typeof(booleanoFalse))
+*/
+
+/*
+Crie duas variáveis e compare os valores: 
+1. a variável x é maior que o variável y? 
+    Se for verdadeira, retornar com a mensagem no console.log(O maior número é x). 
+    Se não for verdadeira, retornar com a mensagem no console.log(O número maior é y);
+    */
+ let x = 10
+ let y = 3
+
+if (x == 10) {
+    console.log ("O maior número é x")
+} else {
+    console.log ("O número maior é y")
+}
+
+
+
+
+
