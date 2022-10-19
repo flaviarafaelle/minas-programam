f14a2d6516e527fcbd30a988ba37f6ef5d46c2f3
Elle <101022170+flaviarafaelle@users.noreply.github.com>
2022-10-19 10:04:12 -0300

    feat: atividadeAula3

--- lógica de programação/atividadesAula3.js
+++ lógica de programação/atividadesAula3.js
@@ -61,7 +61,34 @@ let abaixo = "abaixo"
 if (media == "acima") {
     console.log ("passou de ano bb!")
 } else if (abaixo == "media") {
-    console.log ("passou raspando tal qual os motoqueiros do ifood")
+//    console.log ("passou raspando tal qual os motoqueiros do ifood")
 } else {
-    console.log ("não passou D:")
+//    console.log ("não passou D:")
 }
+
+/*
+Uma comerciante comprou alguns produtos e quer revender com lucro de 45% se o valor da compra foi menor que 20 reais. 
+Caso contrário, o lucro será de 30%. O valor deve ser passado para o programa que deverá retornar o valor da venda.
+
+Exemplo: 
+produto: R$ 18,00 -> 18 * 45% -> R$ 26,10
+produto: R$ 21,00 -> 21 * 30% -> R$ 27,30
+*/
+
+let prod1 = 18
+let prod2 = 21
+let lucro1 = 8.1
+let lucro2 = 6.3
+
+const multiplicacao1 = prod1 + lucro1
+const multiplicacao2 = prod2 + lucro2
+
+if (prod1 > 20) {
+  console.log (`Com lucro de 45% o valor da venda foi ${multiplicacao1}`)
+} else if (prod2 > 20) {
+  console.log (`Com lucro de 30% o valor da venda foi ${multiplicacao2}`)
+} else {
+  console.log ("O comerciante não vendeu")
+}
+
+
