ca58915d087d8db1266acbc56325759f281915a2
Elle <101022170+flaviarafaelle@users.noreply.github.com>
2022-10-21 10:10:58 -0300

    feat: exercicios

---
+++ lógica de programação II/exercicios_extras.js
@@ -0,0 +1,35 @@
+/*
+01. Com base nas tabelas de decisão, determine os resultados lógicos das expressões mencionadas, assinalando se são verdadeiras ou falsas. 
+Considere para as respostas os seguintes valores: 
+
+X = 1
+A = 3
+B = 5
+C = 8
+D = 7
+  
+a) não (X > 3)                 resp: (true)
+b) (X < 1) e não (B > D)       resp: (true)
+c) não (D < 0) e (C > 5)       resp: ()
+d) não (X > 3) ou (C < 7)      resp: ()
+e) (A > B) ou (C > B)          resp: (true)
+f) (X >= 2)                    resp: ()
+g) (X < 1) e (B >=D)           resp: ()
+h) (D < 0) ou (C > 5)          resp: ()
+i) não (D > 3) ou não (B < 7)  resp: ()
+j) (A > B) ou não (C > B)      resp: ()
+
+a) não (1 > 3)                 resp: (true)
+b) (1 < 1) e não (5 > 7)       resp: (false)
+c) não (7 < 0) e (8 > 5)       resp: (true)
+d) não (1 > 3) ou (8 < 7)      resp: (true)
+e) (3 > 5) ou (8 > 5)          resp: (true)
+f) (1 >= 2)                    resp: (false)
+g) (1 < 1) e (5 >=7)           resp: (false)
+h) (7 < 0) ou (8 > 5)          resp: (true)
+i) não (7 > 3) ou não (5 < 7)  resp: (false)
+j) (3 > 5) ou não (8 > 5)      resp: (false)
+*/
+
+
+
