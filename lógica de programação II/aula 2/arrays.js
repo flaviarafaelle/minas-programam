7bce80c0a520eb7deb79f7770e3356bf7a810e64
flavinha <flaviarlimasantos@gmail.com>
2022-10-25 18:10:19 -0300

    feat: arrays

---
+++ lógica de programação II/aula 2/arrays.js
@@ -0,0 +1,80 @@
+/*
+Os Arrays (listas) são usados para armazenar vários valores em uma única variável.
+
+Propriedades e Métodos de um Array:
+O poder dos Arrays de JavaScript são as propriedades e métodos que podemos aplicar
+
+1. Um array de carros
+const marcaCarros = ["Ford", "Hyundai", "Fiat", "BMW", "Etios"]; //Array
+
+2. Acessando elementos de um array através da posição (começa do zero)
+marcaCarros[3];
+
+3. Quantidade de elementos de um array (length)
+marcaCarros.length
+
+4. Adicionar elementos ao final do array (push)
+marcaCarros.push("Honda");
+
+5. Como adicionar um elemento em uma posição específica?
+marcaCarros[5] = ("Honda")
+
+6. Como pegar a última posição do array e adicionar algo nela?
+marcaCarros[marcaCarros.length] = ("Honda")
+
+7. Removendo o elemento da última posição do array
+marcaCarros.pop()
+*/
+
+/*
+01
+const marcaCarros = ["Ford", "Hyundai", "Fiat", "BMW", "Etios"];
+console.log(marcaCarros);
+*/
+
+/*
+02
+const marcaCarros = ["Ford", "Hyundai", "Fiat", "BMW", "Etios"];
+console.log(marcaCarros[3]);
+console.log(marcaCarros);
+*/
+
+/*
+03
+const marcaCarros = ["Ford", "Hyundai", "Fiat", "BMW", "Etios"];
+console.log(marcaCarros.length);
+console.log(marcaCarros);
+*/
+
+/*
+04
+const marcaCarros = ["Ford", "Hyundai", "Fiat", "BMW", "Etios"];
+console.log(marcaCarros.push("Honda"));
+console.log(marcaCarros);
+*/
+
+/*
+05
+const marcaCarros = ["Ford", "Hyundai", "Fiat", "BMW", "Etios"];
+marcaCarros[2] = ("Honda");
+console.log(marcaCarros);
+*/
+
+/*
+06
+const marcaCarros = ["Ford", "Hyundai", "Fiat", "BMW", "Etios"];
+marcaCarros[marcaCarros.length] = ("Tesla");
+console.log(marcaCarros);
+*/
+
+/*
+07
+const marcaCarros = ["Ford", "Hyundai", "Fiat", "BMW", "Etios"];
+marcaCarros.pop();
+console.log(marcaCarros);
+*/
+
+
+
+
+
---
+++ lógica de programação II/aula 2/objetos.js
@@ -0,0 +1,12 @@
+/*
+Um objeto é uma coleção de propriedades.
+Uma propriedade é uma associação entre uma chave e um valor.
+*/
+
+//Criando um objeto
+const aluna = { nome: "Elle", cargo:"Desenvolvedora", empresa: "Arouca"};
+const professora = { nome: "Liz", cargo:"Professora", empresa: "Minas Programam" };
+const diciplinaLP = ["LP1", "LP2", "LP3"];
+
+//Acessando os elementos do objeto
+console.log("A aluna se chama " + aluna.nome + " e a professora se chama " + professora.nome)
