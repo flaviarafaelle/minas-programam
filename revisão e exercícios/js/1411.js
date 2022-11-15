b56beca0ea701c3b4d63e6622e2db3a9f071e405
flavinha <flaviarlimasantos@gmail.com>
2022-11-15 10:25:06 -0300

    feat: revisao

---
+++ revisão e exercícios/js/1411.js
@@ -0,0 +1,11 @@
+/*Deboas
+Crie uma função que recebe por parâmetro o tempo de duração expressa em 
+segundos e retorna esse tempo em horas e minutos.
+conteúdo: operadores aritméticos
+
+Crie uma função que receba um número e usando laços de repetição 
+calcule e mostre a tabuada desse número
+conteúdo: operadores aritméticos e estrutura de repetição
+*/
+
+
